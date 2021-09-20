9e2c03a4abb83037f2f2c928733dc999f65e13f2
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-09-20 16:34:37 +0200

    fetch the data.json file and import components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import './App.css';
 import CreateUser from './components/CreateUser';
-import FilterUsers from './components/FilterUsers';
-import DisplayUsers from './components/UserCard';
+import DisplayUsers from './components/DisplayUsers';
+import usersContext from './context/usersContext/usersContext';
 
 const App = () => {
+  const userState = useContext(usersContext);
+  const { dispatchUsers } = userState;
 
-  return (
-    <div className="App">
-      <DisplayUsers />
-      <FilterUsers />
-      <CreateUser/>
-    </div>
-  );
+  const [rendering, setRendering] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    const doFetch = async () => {
+      const response = await fetch("./data.json");
+      const data = await response.json();
+      if (isMounted) {
+        dispatchUsers({ type: "fetchUsers", payload: data });
+        setRendering(true);
+      }
+    };
+    try {
+      doFetch();
+    } catch (err) {
+      console.log("Unable to fetch config file :", err);
+    }
+
+    return () => {
+      isMounted = false;
+    };
+  }, [dispatchUsers]);
+
+  if (rendering) {
+    return (
+      <div className="App">
+        <DisplayUsers />
+        <CreateUser />
+      </div>
+    );
+  }
+  return null
 }
 
 export default App;
